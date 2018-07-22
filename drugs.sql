9d9739f5ff30d0c23e8c66b6cb284154e25f5fc7
Serhii Kostel <dodge.ksv@gmail.com>
2018-07-22 10:24:48 +0300

    Add create table SQL

---
+++ drugs.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Drugs
+(
+    Name NVARCHAR(127) NOT NULL,
+	Link NVARCHAR(255) NOT NULL,
+	Dosage NVARCHAR(255),
+	Manufacture NVARCHAR(255),
+	INN NVARCHAR(127),
+	PharmGroup NVARCHAR(255),
+	Registration NVARCHAR(127),
+	ATCCode NVARCHAR(1023),
+	Instruction NVARCHAR(MAX),
+);
